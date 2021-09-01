26308962aec3c10bc174374db7a6aaee52c480ef
Iain <iain@zora.co>
2021-09-01 23:06:43 +0100

    strings factory

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,18 @@
 import { HardhatUserConfig } from 'hardhat/config';
+import 'hardhat-gas-reporter';
 import '@nomiclabs/hardhat-ethers';
-import "hardhat-typechain";
+import 'hardhat-typechain';
 
 const config: HardhatUserConfig = {
-  solidity: '0.8.6',
+  solidity: {
+    version: '0.8.6',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 100,
+      },
+    },
+  },
 };
 
 export default config;
